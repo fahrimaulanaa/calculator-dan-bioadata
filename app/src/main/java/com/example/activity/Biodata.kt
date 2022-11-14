8d6f6beae50a32fc8c010e069a03e4277d063914
fahrimaulanaa <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2022-11-14 11:40:47 +0700

    .

--- app/src/main/java/com/example/activity/Biodata.kt
+++ app/src/main/java/com/example/activity/Biodata.kt
@@ -1,117 +1,91 @@
 package com.example.activity
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.app.DatePickerDialog
+import android.widget.DatePicker
+import java.text.SimpleDateFormat
+import java.util.Calendar
 
 class Biodata : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var editNamaDepan: EditText
-    private lateinit var editNamaBelakang: EditText
-    private lateinit var editAlamat: EditText
-    private lateinit var editTelefon: EditText
-    private lateinit var editTanggalLahir: EditText
-    private lateinit var btnSimpan: Button
+    var editDate : EditText? = null
+    var textView : TextView? = null
+    var cal = Calendar.getInstance()
+    private lateinit var etNamaDepan: EditText
+    private lateinit var etNamaBelakang: EditText
+    private lateinit var etTanggalLahir: EditText
+    private lateinit var etAlamat: EditText
+    private lateinit var etTelefon: EditText
+    private lateinit var btnSubmit: Button
     private lateinit var tvResult: TextView
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bioadata)
 
-        editNamaDepan = findViewById(R.id.edt_namaDepan)
-        editNamaBelakang = findViewById(R.id.edt_namaBelakang)
-        editAlamat = findViewById(R.id.edt_alamat)
-        editTelefon = findViewById(R.id.edt_phone)
-        editTanggalLahir = findViewById(R.id.edt_date)
-        btnSimpan = findViewById(R.id.btn_submit)
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView()
+            }
+        }
+
+        etNamaDepan = findViewById(R.id.edt_namaDepan)
+        etNamaBelakang = findViewById(R.id.edt_namaBelakang)
+        etTanggalLahir = findViewById(R.id.edt_date)
+        etAlamat = findViewById(R.id.edt_alamat)
+        etTelefon = findViewById(R.id.edt_phone)
+        btnSubmit = findViewById(R.id.btn_submit)
         tvResult = findViewById(R.id.tv_result)
-        btnSimpan.setOnClickListener(this)
+        btnSubmit.setOnClickListener(this)
+        etTanggalLahir.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                DatePickerDialog(this@Biodata,
+                        dateSetListener,
+                        // set DatePickerDialog to point to today's date when it loads up
+                        cal.get(Calendar.YEAR),
+                        cal.get(Calendar.MONTH),
+                        cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        })
 
+    }
 
-
+    private fun updateDateInView() {
+        val myFormat = "MM/dd/yy" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, java.util.Locale.US)
+        etTanggalLahir.setText(sdf.format(cal.getTime()))
     }
 
     override fun onClick(p0: View?) {
         if (p0?.id == R.id.btn_submit) {
-            val inputNamaDepan = editNamaDepan.text.toString().trim()
-            val inputNamaBelakang = editNamaBelakang.text.toString().trim()
-            val inputAlamat = editAlamat.text.toString().trim()
-            val inputTelefon = editTelefon.text.toString().trim()
-            val inputTanggalLahir = editTanggalLahir.text.toString().trim()
+            val namaDepan = etNamaDepan.text.toString().trim()
+            val namaBelakang = etNamaBelakang.text.toString().trim()
+            val tanggalLahir = etTanggalLahir.text.toString().trim()
+            val alamat = etAlamat.text.toString().trim()
+            val telefon = etTelefon.text.toString().trim()
 
-            var isEmptyFields = false
-            var isInvalidDouble = false
-
-            if (inputNamaDepan.isEmpty()) {
-                isEmptyFields = true
-                editNamaDepan.error = "Field ini tidak boleh kosong"
-            }
-
-            if (inputNamaBelakang.isEmpty()) {
-                isEmptyFields = true
-                editNamaBelakang.error = "Field ini tidak boleh kosong"
-            }
-
-            if (inputAlamat.isEmpty()) {
-                isEmptyFields = true
-                editAlamat.error = "Field ini tidak boleh kosong ya"
-            }
-
-            if (inputTelefon.isEmpty()) {
-                isEmptyFields = true
-                editTelefon.error = "Field ini tidak boleh kosong"
-            }
-
-            if (inputTanggalLahir.isEmpty()) {
-                isEmptyFields = true
-                editTanggalLahir.error = "Field ini tidak boleh kosong"
-            }
-
-            val namaDepan = toString(inputNamaDepan)
-            val namaBelakang = toString(inputNamaBelakang)
-            val alamat = toString(inputAlamat)
-            val telefon = toString(inputTelefon)
-            val tanggalLahir = toString(inputTanggalLahir)
-
-            if (namaDepan == null) {
-                isInvalidDouble = true
-                editNamaDepan.error = "Field ini harus berupa nomer yang valid"
-            }
-
-            if (namaBelakang == null) {
-                isInvalidDouble = true
-                editNamaBelakang.error = "Field ini harus berupa nomer yang valid"
-            }
-
-            if (alamat == null) {
-                isInvalidDouble = true
-                editAlamat.error = "Field ini harus berupa nomer yang valid"
-            }
-
-            if (telefon == null) {
-                isInvalidDouble = true
-                editTelefon.error = "Field ini harus berupa nomer yang valid"
-            }
-
-            if (tanggalLahir == null) {
-                isInvalidDouble = true
-                editTanggalLahir.error = "Field ini harus berupa tanggal yang valid"
-            }
-
-            if (!isEmptyFields && !isInvalidDouble) {
-                val result = "Nama Depan: $namaDepan, Nama Belakang: $namaBelakang, Alamat: $alamat, Nomor Telefon: $telefon, Tanggal Lahir: $tanggalLahir"
-                tvResult.text = result.toString()
-            }
+            tvResult.text = """"
+                Nama Depan : $namaDepan
+                Nama Belakang : $namaBelakang
+                Tanggal Lahir : $tanggalLahir
+                Alamat : $alamat
+                Telefon : $telefon
+            """.trimIndent()
         }
     }
-
-    private fun toString(inputTanggalLahir: String): String {
-        return inputTanggalLahir
-
-    }
-
 }
+
+
+
+
